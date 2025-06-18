3f22e303c54f3a2e6b61a5e184c0bdd2bf92aa75
raeen-fatima <raeenfatimahere@gmail.com>
2025-06-18 10:20:31 +0530

    Initial commit

--- Backend/routes/userRoute.js
+++ Backend/routes/userRoute.js
@@ -1,15 +1,15 @@
-// // routes/userRoutes.js
-// import express from "express";
-// import protect from "../middleware/authMiddleware.js";
+// routes/userRoutes.js
+import express from "express";
+import protect from "../middleware/authMiddleware.js";
 
-// const router = express.Router();
+const router = express.Router();
 
-// // Profile route: This will only be accessible if the user is authenticated
-// router.get("/profile", protect, (req, res) => {
-//   res.status(200).json({
-//     message: "Accessed protected route!",
-//     user: req.user, // User info decoded from the JWT
-//   });
-// });
+// Profile route: This will only be accessible if the user is authenticated
+router.get("/profile", protect, (req, res) => {
+  res.status(200).json({
+    message: "Accessed protected route!",
+    user: req.user, // User info decoded from the JWT
+  });
+});
 
-// export default router;
+export default router;
