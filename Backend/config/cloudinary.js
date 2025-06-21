4bb5d71d25ee888928272612f76172cb9435b99c
raeen-fatima <raeenfatimahere@gmail.com>
2025-06-21 11:03:29 +0530

    initial commit

--- Backend/config/cloudinary.js
+++ Backend/config/cloudinary.js
@@ -4,10 +4,11 @@ import { v2 as cloudinary } from "cloudinary";
 import { CloudinaryStorage } from "multer-storage-cloudinary";
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME || (() => { throw new Error("CLOUDINARY_CLOUD_NAME not set"); })(),
+  api_key: process.env.CLOUDINARY_API_KEY || (() => { throw new Error("CLOUDINARY_API_KEY not set"); })(),
+  api_secret: process.env.CLOUDINARY_API_SECRET || (() => { throw new Error("CLOUDINARY_API_SECRET not set"); })(),
 });
+
 
 const storage = new CloudinaryStorage({
   cloudinary,
